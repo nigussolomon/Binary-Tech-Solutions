8382fb1c9dcfa2e5a8d763124efc97c40a66f6cc
nigussolomon <nigussolomon444@gmail.com>
2022-07-19 19:13:14 +0300

    INITIAL COMMIT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
+import { WorksComponent } from './works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'teams', component: TeamsComponent},
+  {path: 'contacts', component: ContactComponent},
+  {path: 'works', component: WorksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TeamsComponent } from './teams/teams.component';
+import { ContactComponent } from './contact/contact.component';
+import { WorksComponent } from './works/works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    TeamsComponent,
+    ContactComponent,
+    WorksComponent
   ],
   imports: [
     BrowserModule,
